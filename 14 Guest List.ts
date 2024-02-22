b85dd019eabd15f1a3edaedf19d7565ae3b5deaf
SyedWaqasAhmedShah <160391946+SyedWaqasAhmedShah@users.noreply.github.com>
2024-02-22 12:38:16 +0500

    Add files via upload

---
+++ 14 Guest List.ts
@@ -0,0 +1,4 @@
+let Guesr_List :string[] = ['Muzaffer Hussain','Shahzaib Hassan','Abdul Wahab'];
+for(let i=0; i<Guesr_List.length; i++){
+    console.log('Dear Mr. ' + Guesr_List[i] + ',\n\nIt Is Our Pleasure To Invite You In Our Party. \n\nThank You!')
+}
