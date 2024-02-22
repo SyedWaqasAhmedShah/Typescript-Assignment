0834cde9d91474e30136f4fd881bf0b57c430f1b
SyedWaqasAhmedShah <160391946+SyedWaqasAhmedShah@users.noreply.github.com>
2024-02-22 12:41:00 +0500

    Add files via upload

---
+++ 16 More Guests.ts
@@ -0,0 +1,23 @@
+let Guest_List :string[] = ['Muzaffer Hussain','Shahzaib Hassan','Abdul Wahab'];
+//for(let i=0; i<Guest_List.length; i++){
+//  console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt Is Our Pleasure To Invite You In Our Party. \n\nThank You!\n\n')
+//}
+
+let absent_Guest :string = 'Abdul Wahab';
+let new_Guest : string ='Fayaz Ali';
+Guest_List[0] = new_Guest;
+
+for(let i=0; i<Guest_List.length; i++){
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt Is Our Pleasure To Invite You In Our Party. \n\nThank You!\n\n')
+}
+
+console.log(`Mr. ${absent_Guest} is not coming to the party.`);
+
+console.log('Good News! We Find Big Table So We Are Inviting 3 More Guests.')
+Guest_List.unshift('Hasnain');
+Guest_List.splice(2 , 0 , 'Zakir');
+Guest_List.push('Abdul Hanan');
+
+for(let i=0; i<Guest_List.length; i++){
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt Is Our Pleasure To Invite You In Our Party. \n\nThank You!\n\n')
+}
