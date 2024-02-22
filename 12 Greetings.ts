ebd88016726346b1798a81a3fb0e4164d9ed14c6
SyedWaqasAhmedShah <160391946+SyedWaqasAhmedShah@users.noreply.github.com>
2024-02-22 12:35:43 +0500

    Add files via upload

---
+++ 12 Greetings.ts
@@ -0,0 +1,5 @@
+let names :string[] = ['Syed Waqas','Muzaffer','Abdul Wahab','Shahzaib'];
+const message :string = 'Welcome To Typescript Class:'
+for(let i=0; i<names.length; i++){
+    console.log(message + names[i]);
+}
