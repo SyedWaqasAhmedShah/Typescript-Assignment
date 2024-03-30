66cd0a435b1106539def2677334f7194614bb7f6
SyedWaqasAhmedShah <160391946+SyedWaqasAhmedShah@users.noreply.github.com>
2024-03-30 09:20:54 +0500

    Add files via upload

---
+++ 45_Cars.ts
@@ -0,0 +1,22 @@
+// 45. Cars: Write a function that stores information about a car in a Object.
+// The function should always receive a manufacturer and a model name. It
+// should then accept an arbitrary number of keyword arguments. Call the function
+// with the required information and two other name-value pairs, such as a
+// color or an optional feature. 
+// Print the Object that’s returned to make sure all the information was
+// stored correctly.
+
+import { features } from "process";
+
+function storeCarInfo(manufacturer:string, modelName:string, ...extraoption: { [key : string]: any } [] ):object{
+    const carinfo = {
+        manufacturer,
+        modelName,
+        ...Object.assign({}, ...extraoption)
+    }
+    return carinfo;
+};
+
+let answer = storeCarInfo("Toyota","Corolla", {color:"white"}, {features:["navigation", "power window"]})
+
+console.log(answer);
